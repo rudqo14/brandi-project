d0fa66862cad7af4b00c9e8acb7e98c26c04d89e
junggyoo <jungggyu.1@gmail.com>
2020-08-23 22:57:29 +0900

    [초기세팅 > 프론트엔드]  - Webpack 개발모드, 배포모드 추가

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,12 +1,11 @@
 const VueLoaderPlugin = require("vue-loader/lib/plugin");
 const path = require("path");
+const env = process.env.ENV || "development";
+// 개발모드와 배포모드 설정을 분리하기 위해 사용 
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
-module.exports = {
-  mode: "development",
-  devtool: "eval",
-  devServer: {
-    historyApiFallback: true,
-  },
+const config = {
+  mode: env,
   resolve: {
     extensions: [".js", ".vue"],
   },
@@ -33,10 +32,41 @@ module.exports = {
       },
     ],
   },
-  plugins: [new VueLoaderPlugin()],
+  plugins: [
+    new VueLoaderPlugin()
+  ],
   output: {
-    filename: "[name].js",
+    filename: `[name].js`,
     path: path.join(__dirname, "dist"),
     publicPath: "/dist",
   },
 };
+
+if (env === "development") {
+  // 개발환경에서만 웹팩 devtool 을 활성화한다. 
+
+  config.devtool = "eval";
+  config.devServer = {
+    historyApiFallback: true,
+  };
+
+  // 배포환경에서만 css 를 style.css 로 따로 번들링한다. 
+} else if (env === "production") {
+  config.module.rules = [
+    {
+      test: /\.vue$/,
+      use: "vue-loader",
+    },
+    {
+      test: /\.(sa|sc|c)ss$/,
+      use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+    },
+  ];
+
+
+  config.plugins.push(new MiniCssExtractPlugin({
+    filename: 'style.css'
+  }));
+}
+
+module.exports = config;
