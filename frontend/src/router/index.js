35c75409f4e5f4e6f1ec74de6fd0c1af0449861f
KIM <rudqo14@gmail.com>
2020-08-24 01:04:44 +0900

    [서비스 페이지 > 로그인 페이지]  - 백엔드로 부터 받아온 access_token값을 로컬스토리지에 저장

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,8 +1,8 @@
-import Vue from "./node_modules/vue";
-import VueRouter from "./node_modules/vue-router";
-import Main from "../BrandiService/Main.vue";
-import Detail from "../BrandiService/Detail.vue";
-import SignIn from "../BrandiService/SignIn/SignIn.vue";
+import Vue from "../../node_modules/vue";
+import VueRouter from "../../node_modules/vue-router";
+import Main from "../BrandiService/Main/Main.vue";
+import Detail from "../BrandiService/Detail/Detail.vue";
+import Login from "../BrandiService/Login/Login.vue";
 
 Vue.use(VueRouter);
 
@@ -19,9 +19,10 @@ export const router = new VueRouter({
     },
     {
       path: "/login",
-      component: SignIn,
+      component: Login,
     },
     {
+      //초기 url을 main으로 적용
       path: "/",
       redirect: "/main",
     },
