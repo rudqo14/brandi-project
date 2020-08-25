ecff56b4aa8459c729afe256954167eef77bfd63
soochung sohn <soochungsohn@sujeong-ui-MacBookPro.local>
2020-08-25 20:07:34 +0900

    Modify      : view > controller 이름 변경 config.py   : time_zone 셋팅 추가

---
+++ backend/controller/__init__.py
@@ -0,0 +1,10 @@
+from .user_controller import create_user_endpoints, create_admin_user_endpoints
+from .order_controller import create_admin_order_endpoints
+from .product_controller import AdminProductView
+
+__all__ = [
+    'create_user_endpoints',
+    'create_admin_user_endpoints',
+    'create_admin_order_endpoints',
+    'AdminProductView'
+]
