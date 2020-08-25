8f46183588a255d0f1b3aead849b821b01a42234
KIM <rudqo14@gmail.com>
2020-08-25 16:10:32 +0900

    [서비스 > 상품 상세 페이지]  - 목데이터 적용하여 레이아웃 구현  - 이미지 슬라이더 적용  - 토글 기능 적용하여 상품(상세/사이즈) 옵션 기능 적용  - 구매 정보 박스에서 상품 수량 조절 버튼 기능 적용

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -3,7 +3,10 @@ import VueRouter from "../../node_modules/vue-router";
 import Main from "../BrandiService/Main/Main.vue";
 import Detail from "../BrandiService/Detail/Detail.vue";
 import Login from "../BrandiService/Login/Login.vue";
+import Banner from "../BrandiService/Components/Banner.vue";
+import VueAgile from "vue-agile";
 
+Vue.use(VueAgile);
 Vue.use(VueRouter);
 
 export const router = new VueRouter({
@@ -20,6 +23,10 @@ export const router = new VueRouter({
     {
       path: "/login",
       component: Login,
+    },
+    {
+      path: "/banner",
+      component: Banner,
     },
     {
       //초기 url을 main으로 적용
