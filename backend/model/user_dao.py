f711589aa0edf241394bcd78171fb1aadb37d859
soochung sohn <soochungsohn@sujeong-ui-MacBookPro.local>
2020-09-08 19:31:04 +0900

    [ 서비스 > 마이페이지 ] - 마이페이지 레이아웃변경 - dao 변경

--- backend/model/user_dao.py
+++ backend/model/user_dao.py
@@ -493,6 +493,7 @@ class UserDao:
 
             select_user_orders = """
             SELECT
+                P1.order_no,
                 P2.order_detail_no,
                 P2.start_time,
                 P7.image_small,
@@ -543,7 +544,7 @@ class UserDao:
 
             WHERE P1.user_id = %(user_no)s
 
-            ORDER BY P1.order_no
+            ORDER BY P1.order_no DESC
             """
 
             cursor.execute(select_user_orders, user_info)
